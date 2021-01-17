62a1a91a307c141f7f99dc1acb0e8c2844ee6c94
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-01-18 00:05:50 +0530

    os_module

---
+++ reference/os_demo.js
@@ -0,0 +1,12 @@
+const os = require('os')
+
+console.log(os.platform());
+console.log(os.release());
+console.log(os.userInfo());
+console.log(os.version());
+console.log(os.arch());
+console.log(os.cpus());
+console.log(os.freemem());
+console.log(os.totalmem());
+console.log(os.homedir());
+console.log(os.uptime()/3600);
