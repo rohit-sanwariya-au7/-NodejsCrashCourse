f2141ee8e1a7f51f3d509b54a9a9d11a65331ec0
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2021-01-17 18:28:28 +0530

    exports

---
+++ Person.js
@@ -0,0 +1,12 @@
+class ClassPerson{
+    constructor(name,age){this.name=name;
+    this.age=age;}
+}
+
+
+const Person = {
+    name:"rohit sanwariya",
+    age:30
+};
+
+module.exports = { Person,ClassPerson};
---
+++ index.js
@@ -0,0 +1,7 @@
+const {Person,ClassPerson} = require('./Person')
+
+const classPerson = new ClassPerson("Pikachu",25);
+console.log(Person.name);
+console.log(classPerson.name);
+console.log(__dirname);
+console.log(__filename);
